ceb30218e86250679ec5bc9a1e1a47784ac099cf
Jaroslaw J. G. Granat <jaroslaw.granat@tpvaoc.com>
2020-09-24 22:33:13 +0200

    Dodaj pliki projektów.

---
+++ ProductionPlanWsp/Server/Controllers/lanningProductionController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProductionPlanWsp.Server.Data;
+using ProductionPlanWsp.Shared;
+
+namespace ProductionPlanWsp.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class planningProductionController : ControllerBase
+    {
+        private readonly PlanowanieContext _context;
+
+        public planningProductionController(PlanowanieContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<VwPlanningProduction>>> planningProduction()
+        {
+            return await _context.VwPlanningProduction.ToListAsync();
+        }
+    }
+}
